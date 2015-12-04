735427d6f88cdf96a81f910074e130c4152a2531
Benjamin Collins <seichestream@gmail.com>
2015-12-04 22:41:56 +0900

    version 0.0.4 small interface change

--- filepointer.js
+++ filepointer.js
@@ -1,18 +1,45 @@
+/*--------------------------------------------------------------------------
+  FilePointer a small wrapper class for reading binary files
+  Copyright (C) 2015  Benjamin Collins
+
+  This utility is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 2 of the License, or
+  (at your option) any later version.
+
+  This utility is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this utility.  If not, see <http://www.gnu.org/licenses/>.
+--------------------------------------------------------------------------*/
+
 var fs = require("fs");
 
-module.exports = function(filename){
+module.exports = function(file){
   this.fp = 0;
-  this.buffer = fs.readFileSync(filename);
+  if(Buffer.isBuffer(file)){
+    this.buffer = file;
+  }else{
+    this.buffer = fs.readFileSync(file);
+  }
+
+  this.is_zero = function(){
+    var integer = this.buffer.readUInt32LE(this.fp);
+    return !integer;
+  }
 
   this.seek_set = function(offset){
     this.fp = 0;
     this.fp += offset;
   }
-  
+
   this.seek_cur = function(offset){
     this.fp += offset;
   }
-  
+
   this.seek_end = function(offset){
     this.fp = this.buffer.length;
     this.fp += offset;
@@ -23,8 +50,20 @@ module.exports = function(filename){
     this.fp = 0;
   }
 
-  this.slice = function(start, end){
-    var tmp = new this.buffer(end - start);
+  this.read_byte = function(){
+    var integer = this.buffer.readUInt8(this.fp);
+    this.fp += 1;
+    return integer;
+  }
+
+  this.copy = function(start, end){
+
+    if(arguments.length == 1){
+      end = this.fp + start;
+      start = this.fp;
+    }
+
+    var tmp = new Buffer(end - start);
     this.buffer.copy(tmp, 0, start, end);
     return tmp;
   }
@@ -55,10 +94,17 @@ module.exports = function(filename){
     return str;
   }
 
-  this.read_str = function(len){
+  this.read_str = function(len, cut){
     var str = this.buffer.toString("ascii", this.fp, this.fp + len);
+
+    if(cut){
+      var end = str.indexOf("\0");
+      str = str.substr(0, end);
+    }else{
+      str = str.replace(/\0/g, "");
+    }
+
     this.fp += len;
-    str = str.replace(/\0/g, "");
     return str;
   }
 
@@ -100,6 +146,85 @@ module.exports = function(filename){
     var angle = angle / 0xFFFF;
     angle = parseInt(360 * angle);
     return angle;
+  }
+
+  this.lz77_decomp = function (data){
+    var self = {
+      ibuf : data,
+      obuf : [],
+      iofs : 0,
+      bit : 0,
+      cmd : 0,
+      getByte : int_getByte,
+      getBit : bool_getBit
+    }
+
+    var t, a, b, j, cmd;
+    var offset, amount, start;
+
+    while (self.iofs < self.ibuf.length){
+      cmd = self.getBit();
+      if(cmd){
+        self.obuf.push(self.ibuf[self.iofs]);
+        self.iofs += 1;
+      }else{
+        t = self.getBit();
+        if(t){
+          a = self.getByte();
+          b = self.getByte();
+
+          offset = ((b << 8) | a) >> 3;
+          amount = a & 7;
+          if (self.iofs < self.ibuf.length){
+            if (amount == 0)
+              amount = self.getByte() + 1;
+            else
+              amount += 2;
+          }
+          start = self.obuf.length - 0x2000 + offset;
+        }else{
+          amount = 0;
+          for (j = 0; j < 2; j++){
+            amount <<= 1;
+            amount |= self.getBit();
+          }
+          offset = self.getByte();
+          amount += 2;
+
+          start = self.obuf.length - 0x100 + offset;
+        }
+        for(j = 0; j < amount; j++){
+          if (start < 0)
+            self.obuf.push(0);
+          else if (start < self.obuf.length)
+            self.obuf.push(self.obuf[start]);
+          else
+            self.obuf.push(0);
+
+          start += 1;
+        }
+      }
+    }//end while
+
+    return new Buffer(self.obuf);
+
+    function int_getByte(){
+      var val = self.ibuf[self.iofs];
+      self.iofs += 1;
+      return parseInt(val);
+    }
+
+    function bool_getBit(){
+      if(self.bit == 0){
+  	     self.cmd = self.getByte()
+         self.bit = 8
+      }
+      var bit = self.cmd & 1;
+      self.cmd >>= 1;
+  	  self.bit -= 1;
+  	  return parseInt(bit);
+    }
+
   }
 
 }
