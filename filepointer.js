c2898e8eca5146c5375052ad12b0ad5fbea692de
seiche <seichestream@gmail.com>
2016-06-01 13:44:45 +0900

    Update filepointer.js

--- filepointer.js
+++ filepointer.js
@@ -20,78 +20,100 @@
 
 var fs = require("fs");
 
-module.exports = class  {
+module.exports = class {
 
-	constructor(file, is_big_endian){
+	constructor(file, is_big_endian) {
 		this.fp = 0;
 
-		if(Buffer.isBuffer(file)){
+		if (Buffer.isBuffer(file)) {
 			this.buffer = file;
-		}else{
+		} else {
 			this.buffer = fs.readFileSync(file);
 		}
 
 		this.is_big_endian = Boolean(is_big_endian);
+		this.len = this.buffer.length;
 	}
-	
+
 	/**
 	 * Seek Functions
 	 **/
 
-	seek_set(pos){
-		if(pos !== parseInt(pos, 10)){
+	seek_set(pos) {
+		if (pos !== parseInt(pos, 10)) {
 			throw new Error("Must provide an integer value to seek_set");
 		}
 
 		this.fp = pos;
 	}
 
-	seek_cur(pos){
-		if(pos !== parseInt(pos, 10)){
+	seekSet(pos){
+		this.seek_set(pos);
+	}
+
+	seek_cur(pos) {
+		if (pos !== parseInt(pos, 10)) {
 			throw new Error("Must provide an integer value to seek_cur");
 		}
 
 		this.fp += pos;
 	}
 
-	seek_end(pos){
-		if(pos !== parseInt(pos, 10)){
+	seekCur(pos){
+		this.seek_cur(pos);
+	}
+
+	seek_end(pos) {
+		if (pos !== parseInt(pos, 10)) {
 			throw new Error("Must provide an integer value to seek_end");
 		}
 
 		this.fp = this.buffer.length + pos;
 	}
 
+	seekEnd(pos){
+		this.seek_end(pos);
+	}
+
+	seek_next(do_seek) {
+		while (!this.buffer.readUInt8(this.fp)) {
+			this.fp += 1;
+			if (this.fp === this.buffer.length - 1) {
+				break;
+			}
+		}
+	}
+
 	/**
 	 * Buffer functions
 	 **/
 
-	trim(){
+	trim() {
 		this.buffer = this.buffer.slice(this.fp);
 		this.fp = 0;
 	}
 
-	copy(len){
+	copy(len) {
 		var tmp = new Buffer(len);
 
-		this.buffer.copy(tmp,0, this.fp, this.fp + len);
+		this.buffer.copy(tmp, 0, this.fp, this.fp + len);
 		this.fp += len;
 
 		return tmp;
 	}
 
-	copy_abs(start, end){
+	copy_abs(start, end) {
 		var len, tmp;
 
 		len = end - start;
 
 		tmp = new Buffer(len);
-		this.buffer.copy(tmp,0, start, end);
+		this.buffer.copy(tmp, 0, start, end);
 		return tmp;
 	}
 
-	is_zero(){
-		if(this.fp >= this.buffer.length -4){
+	is_zero() {
+		if (this.fp >= this.buffer.length - 4) {
 			return false;
 		}
 
@@ -100,20 +122,19 @@ module.exports = class  {
 		return Boolean(integer);
 	}
 
-	get_pos(debug){
-		if(!debug){
-			return this.fp;
-		}else{
-			return this.fp.toString(16);
-		}
+	get_pos() {
+		return this.fp;
 	}
 
-	tell(){
-		var pos = this.fp.toString(16);
-		console.log("Current position: %s", pos.toString(16));
+	tell() {
+		return this.fp;
 	}
 
-	get_len(){
+	get_len() {
+		return this.buffer.length;
+	}
+
+	len() {
 		return this.buffer.length;
 	}
 
@@ -121,165 +142,240 @@ module.exports = class  {
 	 * Read Functions
 	 **/
 
-	 read_byte(){
-	 	var integer;
+	// reads unsigned char (1 bytes)
+	read_byte() {
+		var integer;
 		integer = this.buffer.readUInt8(this.fp);
 		this.fp += 1;
 		return integer;
-	 }
+	}
 
-	 read_word(){
+	//@alias - read_byte (1 bytes)
+	readByte() {
+		return this.read_byte();
+	}
+
+	// reads unsigned short (2 bytes)
+	read_word() {
 		var integer;
 
-		if(!this.is_big_endian){
+		if (!this.is_big_endian) {
 			integer = this.buffer.readUInt16LE(this.fp);
-		}else{
+		} else {
 			integer = this.buffer.readUInt16BE(this.fp);
 		}
 
 		this.fp += 2;
 		return integer;
-	 }
+	}
 
-	 read_ushort(){
+	//@alias - read_word (2 bytes)
+	read_ushort() {
+		return this.read_word();
+	}
+
+	//@alias - read_word (2 bytes)
+	readUShort() {
+		return this.read_word();
+	}
+
+	// reads signed short (2 bytes)
+	read_short() {
 		var integer;
 
-		if(!this.is_big_endian){
-			integer = this.buffer.readUInt16LE(this.fp);
-		}else{
-			integer = this.buffer.readUInt16BE(this.fp);
-		}
-
-		this.fp += 2;
-		return integer;
-	 }
-
-	 read_short(){
-		var integer;
-
-		if(!this.is_big_endian){
+		if (!this.is_big_endian) {
 			integer = this.buffer.readInt16LE(this.fp);
-		}else{
+		} else {
 			integer = this.buffer.readInt16BE(this.fp);
 		}
 
 		this.fp += 2;
 		return integer;
-	 }
-	
-	 read_dword(){
+	}
+
+	//@alias - read_short (2 bytes)
+	readShort() {
+		return this.read_short()
+	}
+
+	// reads unsigned int (4 bytes)
+	read_dword() {
 		var integer;
 
-		if(!this.is_big_endian){
+		if (!this.is_big_endian) {
 			integer = this.buffer.readUInt32LE(this.fp);
-		}else{
+		} else {
 			integer = this.buffer.readUInt32BE(this.fp);
 		}
 
 		this.fp += 4;
 		return integer;
-	 }
+	}
 
-	 read_uint(){
+	// reads unsigned int (4 bytes)
+	read_uint() {
+		return this.read_dword();
+	}
+
+	// reads unsigned int (4 bytes)
+	readUInt() {
+		return this.read_dword();
+	}
+
+	// reads signed int (4 bytes)
+	read_int() {
 		var integer;
 
-		if(!this.is_big_endian){
-			integer = this.buffer.readUInt32LE(this.fp);
-		}else{
-			integer = this.buffer.readUInt32BE(this.fp);
-		}
-
-		this.fp += 4;
-		return integer;
-	 }
-
-	 read_angle(degrees){
-		var integer;
-
-		if(!this.is_big_endian){
-			integer = this.buffer.readUInt32LE(this.fp);
-		}else{
-			integer = this.buffer.readUInt32BE(this.fp);
-		}
-
-		var c = 360 / 0xFFFF;
-		integer = (integer & 0xFFFF) * c;
-
-		this.fp += 4;
-
-		if(degrees){
-			return integer;
-		}else{
-			return (integer * Math.PI) / 180;
-		}
-	 }
-
-	 read_int(){
-		var integer;
-
-		if(!this.is_big_endian){
+		if (!this.is_big_endian) {
 			integer = this.buffer.readInt32LE(this.fp);
-		}else{
+		} else {
 			integer = this.buffer.readInt32BE(this.fp);
 		}
 
 		this.fp += 4;
 		return integer;
-	 }
+	}
 
-	 read_single(){
+	//@alias - read_int (4 bytes)
+	readInt() {
+		return this.read_int();
+	}
+
+	// reads single point float (4 bytes)
+	read_single() {
 		var float;
 
-		if(!this.is_big_endian){
+		if (!this.is_big_endian) {
 			float = this.buffer.readFloatLE(this.fp);
-		}else{
+		} else {
 			float = this.buffer.readFloatBE(this.fp);
 		}
 
 		this.fp += 4;
 		return float;
-	 }
+	}
+
+	//@alias - read_single (4 bytes)
+	read_float() {
+		return this.read_single();
+	}
+
+	//@alias - read_single (4 bytes)
+	readFloat() {
+		return this.read_single();
+	}
+
+	//reads int as angle value (4 bytes)
+	read_angle(degrees) {
+		var rawInt = this.read_int();
+		return rawInt * 360 / 0xFFFF;
+	}
+
+	//@alias - read_angle (4 bytes)
+	readAngle() {
+		return this.read_Angle();
+	}
+
+	//reads int as radian value (4 bytes)
+	read_rad() {
+		return this.read_angle() * Math.PI / 180;
+	}
+
+	//@alias - read_rad (4 bytes)
+	readRad() {
+		return this.read_rad();
+	}
+
+	//reads three angles (x,y,z) (12 bytes)
+	readAng3() {
+		return [
+			this.read_angle().toFixed(2),
+			this.read_angle().toFixed(2),
+			this.read_angle().toFixed(2)
+		];
+	}
+
+	//reads three radians (x,y,z) (12 bytes)
+	readRot3() {
+		return [
+			this.read_rad(),
+			this.read_rad(),
+			this.read_rad()
+		];
+	}
+
+	//reads three floats (x,y,z) (12 bytes)
+	readVec3() {
+		return [
+			this.read_single(),
+			this.read_single(),
+			this.read_single()
+		];
+	}
+
 
 	/**
-	* String Functions
-	**/
+	 * String Functions
+	 **/
 
-	read_str(len){
+	read_str(len) {
 		var str, null_index;
 
-		str = this.buffer.toString("ascii", this.fp, this.fp + len);
-		this.fp += len;
-		
+		if (typeof len == 'undefined') {
+			var pos = this.fp;
+
+			while(this.buffer[this.fp]){
+				this.fp++;
+			}
+
+			str = this.buffer.toString("ascii", pos, this.fp);
+		} else {
+			str = this.buffer.toString("ascii", this.fp, this.fp + len);
+			this.fp += len;
+		}
+
 		null_index = str.indexOf("\0");
-		if(null_index !== -1){
+		if (null_index !== -1) {
 			str = str.substr(0, null_index);
 		}
 
 		return str;
 	}
 
-	read_iff(){
+	readString(len) {
+		return this.read_str(len);
+	}
+
+	readStr(len) {
+		return this.read_str(len);
+	}
+
+	read_iff() {
 		var str, null_index;
 
 		str = this.buffer.toString("ascii", this.fp, this.fp + 4);
 		this.fp += 4;
-		
+
 		null_index = str.indexOf("\0");
-		if(null_index !== -1){
+		if (null_index !== -1) {
 			str = str.substr(0, null_index);
 		}
 
 		return str;
 	}
 
-	find(match){
-		var str, search_len;
-		
-		search_len= this.buffer.length - match.length;
+	readIff(){
+		return this.read_iff();
+	}
 
-		for(var pos = this.fp; pos < search_len; pos++){
+	find(match) {
+		var str, search_len;
+
+		search_len = this.buffer.length - match.length;
+
+		for (var pos = this.fp; pos < search_len; pos++) {
 			str = this.buffer.toString("ascii", pos, pos + match.length);
-			if(str.localeCompare(match) === 0){
+			if (str.localeCompare(match) === 0) {
 				this.fp = pos;
 				return true;
 			}
@@ -289,4 +385,3 @@ module.exports = class  {
 	}
 
 }
-
